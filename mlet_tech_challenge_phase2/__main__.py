cfe1e41f8d2deda385e2857f92acb598c7e65f69
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2024-07-09 01:48:35 -0300

    Parse data from current day

--- mlet_tech_challenge_phase2/__main__.py
+++ mlet_tech_challenge_phase2/__main__.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from datetime import datetime
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 # URL para mapear o endereço de consulta do pregão da B3
 B3_AJUSTES_DO_PREGAO = "https://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/historico/derivativos/ajustes-do-pregao/"
+
 
 def setup_driver():
     """Setup selenium webdriver"""
@@ -30,32 +30,44 @@ def setup_driver():
 
     return driver
 
-b3_data = {}
 
 def main() -> int:
     driver = setup_driver()
     driver.get(B3_AJUSTES_DO_PREGAO)
     assert "Ajustes do pregão | B3" in driver.title
-    iframe = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.ID, "bvmf_iframe")))[0]
+    iframe = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.ID, "bvmf_iframe")),
+    )[0]
     driver.switch_to.frame(iframe)
     table = driver.find_element(By.XPATH, "//table/tbody")
+
+    # Each tr maps the current possibilities. It will be just tr -> td[n], tr -> td[n]
+    # The first td[0] can be either the product or just a blank line that contains some other data that must be in a list
     rows = table.find_elements(By.TAG_NAME, "tr")
 
+    current_header = ""
+    previous_header = ""
+    b3_data = {}
     for row in rows:
-        current_data = []
-        current_header = ""
-        for a in row.find_elements(By.TAG_NAME, "td"):
-            has_rowspan = a.get_dom_attribute("rowspan") is not None
+        data = [a.text for a in row.find_elements(By.TAG_NAME, "td")]
+        # It means that we have a header in here
+        if len(data) == 6:
+            current_header = data[0]
+            # We don't care about the name in the data, only to take it as a header
+            data.pop(0)
 
-            if has_rowspan:
-                current_header = a.text
-            else:
-                current_data.append(a.text)
+        if current_header not in b3_data:
+            b3_data[current_header] = []
 
-        b3_data.update({current_header: current_data})
+        b3_data[current_header].append(data)
 
-    print(b3_data)
+        if current_header != previous_header:
+            previous_header = current_header
+
+    # pprint.pprint(b3_data)
+    print(len(b3_data))
     return 0
+
 
 if __name__ == "__main__":
     main()
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,4 +1,5 @@
 from mlet_tech_challenge_phase2.__main__ import main
 
+
 def test_main():
     assert main() == 0
